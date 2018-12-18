1d38e9331e8ecd17847acfe54050dc71d5633119
aldo <aldo@brounie.com>
2018-12-17 21:19:01 -0600

    [RODRIGO] Commit initial

---
+++ routes/login.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page.
+
+709306ff869641d2b43ab576041a0baa
+
+*/
+var controller = {
+  index:function(req,res,next){
+    var context = {
+      user:req.session.user,
+      title:"Express"
+    }
+    res.render('index', context);
+  },
+}
+
+router.get("/",auth.isAuth, controller.index); 
+
+module.exports = router;
