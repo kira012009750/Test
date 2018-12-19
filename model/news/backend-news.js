30bca3d4d8cba21b65d9df80d64c4a3aa1ef6f21
aldo@brounie.com <aldo@brounie.com>
2018-12-18 20:03:32 -0600

    [TRABAJANDO EN CAMBIOS]

--- model/news/backend-news.js
+++ model/news/backend-news.js
@@ -2,9 +2,28 @@
  var NewsAPI = require('newsapi');
  var newsapi = new NewsAPI('709306ff869641d2b43ab576041a0baa');
 
-function list(handler){
-    // To query /v2/top-headlines
-    // All options passed to topHeadlines are optional, but you need to include at least one of them
+function all(handler){
+    newsapi.v2.topHeadlines({
+        language: 'en',
+        sortBy: 'relevancy',
+    }).then(response => {
+        handler({status:200,msn:"This are the news",news:response});
+    }).catch(error =>{
+        handler({status:500,msn:"There was a mistake",error:error});
+    });
+}
+
+function sources(handler){
+    newsapi.v2.sources({
+        language: 'en',
+      }).then(response => {
+        handler({status:200,msn:"This are the sources",sources:response});
+    }).catch(error =>{
+        handler({status:500,msn:"There was a mistake",error:error});
+    });
+}
+
+function allUsa(handler){
     newsapi.v2.topHeadlines({
         language: 'en',
         country: 'us',
@@ -16,6 +35,38 @@ function list(handler){
     });
 }
 
+function findBySource(form,handler){
+    console.log("FORM",form);
+    newsapi.v2.topHeadlines({
+        language: 'en',
+        sources: form.source,
+        sortBy: 'relevancy',
+    }).then(response => {
+        console.log("SEARCH-BY-SOURCE",response);
+        handler({status:200,msn:"This are the news",news:response});
+    }).catch(error =>{
+        handler({status:500,msn:"There was a mistake",error:error});
+    });
+}
+
+function findByKeyWord(form,handler){
+    console.log("FORM",form);
+    newsapi.v2.topHeadlines({
+        language: 'en',
+        q: form.word,
+        sortBy: 'relevancy',
+    }).then(response => {
+        console.log("SEARCH-BY-KEYWORD",response);
+        handler({status:200,msn:"This are the news",news:response});
+    }).catch(error =>{
+        handler({status:500,msn:"There was a mistake",error:error});
+    });
+}
+
 module.exports = {
-    list:list,
+    all:all,
+    allUsa:allUsa,
+    findBySource:findBySource,
+    findByKeyWord:findByKeyWord,
+    sources:sources,
 };
--- routes/home.js
+++ routes/home.js
@@ -10,15 +10,45 @@ var controller = {
       name:req.session.name,
       title:"Home"
     }
-    news.list(function(response){
-      context.news = response.news;
-      context.newsJS = JSON.stringify(response.news);
-      res.render('home', context);
-    })
-    
+    res.render('home', context);
   },
+
+  all:function(req,res,next){ //---GET ALL NEWS 
+    news.all(function(response){
+      res.status(response.status).send(response);
+    });
+  },
+
+  allUsa:function(req,res,next){
+    news.allUsa(function(response){
+      res.status(response.status).send(response);
+    });
+  },
+
+  categories:function(req,res,next){ //---GET ALL CATEGORIES
+    news.sources(function(response){
+      res.status(response.status).send(response);
+    });
+  },
+
+  source:function(req,res,next){
+    news.findBySource(req.body,function(response){
+      res.status(response.status).send(response);
+    });
+  },
+
+  keyword:function(req,res,next){
+    news.findByKeyWord(req.body,function(response){
+      res.status(response.status).send(response);
+    });
+  }
 }
 
 router.get("/",auth.isAuth, controller.index); 
+router.get("/all",auth.isAuth, controller.all); 
+router.get("/allUsa",auth.isAuth, controller.allUsa); 
+router.get("/categories",auth.isAuth, controller.categories); 
+router.post("/source",auth.isAuth, controller.source); 
+router.post("/keyword",auth.isAuth, controller.keyword); 
 
 module.exports = router;
